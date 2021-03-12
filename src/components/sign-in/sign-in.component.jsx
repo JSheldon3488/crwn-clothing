192a9f61c158e6f1699c6cb7367b58ea2abfd6c7
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-12 17:47:06 -0600

    Implemented Sign-Out features

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,8 +49,10 @@ class SignIn extends React.Component {
                         required 
                         handleChange={this.handleChange}
                     />
-                    <CustomButton type='submit'> Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'> Sign In </CustomButton>
+                        <CustomButton isGoogleSignIn onClick={signInWithGoogle}>Sign In With Google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
