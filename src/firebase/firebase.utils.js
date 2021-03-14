cbf562cbfb43a6c94c74868ba691a97bf19d5e73
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-14 12:36:04 -0500

    Created and styled SignUp Component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,8 +12,34 @@ const config = {
     measurementId: "G-XZSZR6RM13"
   };
 
-  firebase.initializeApp(config);
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+      if (!userAuth) return;
 
+      //Check if user document already exists
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
+      const snapShot = await userRef.get();
+      
+      // No user data so set up a document
+      if (!snapShot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (err) {
+            console.log('error creating user', err.message);
+        }
+      }
+
+      return userRef;
+    }
+
+    
+  firebase.initializeApp(config);
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
