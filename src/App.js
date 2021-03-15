be4822bcadd46eab3f1994451651938e5b1ed46d
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-15 16:28:31 -0500

    Setup Redux and  User Reducer

--- src/App.js
+++ src/App.js
@@ -34,10 +34,11 @@ class App extends React.Component {
               ...snapShot.data()
             }
           })
-          console.log(this.state);
         });
       }
-      this.setState({ currentUser: userAuth});
+      else {
+        this.setState({ currentUser: userAuth});
+      }
     });
   }
 
