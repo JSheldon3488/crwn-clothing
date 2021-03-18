1e86c6b699e1642159e8f0ab9a1a7282a8001ea0
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-18 12:57:04 -0500

    Added increase and decrease quantity functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     // Cart item not already in card to add new cartItem object with quantity 1
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const quantity = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id).quantity
+
+    if (quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+    
+    return cartItems.map(cartItem => cartItem.id === cartItemToRemove.id ?
+        {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    )
+}
