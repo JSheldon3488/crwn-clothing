036bd28268df50669d24dc642c9d43063defe856
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-20 11:25:18 -0500

    Added Stripe Integration for Payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price*100;
+    const publishableKey = 'pk_test_51IX7FcHhkqHIEjl3BDyq38hsKJoUX2TGJD7Wq34JrSNEdTlemwYiKA85OsUF1Hwow3nnvTHX9oX2ogGaBXkFEBkn00iKTcuLD0';
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clother Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
