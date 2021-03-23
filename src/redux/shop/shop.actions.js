ed87bb66f342c1cae5062d2c926f4b72e2d2d8e8
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-23 16:10:38 -0500

    Moved thunk functionality for shop data  into a redux saga

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,31 +1,15 @@
 import ShopActionTypes from './shop.types';
 
-import { firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
-
 export const fetchCollectionsStart = () => ({
     type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
 
-const fetchCollectionsSuccess = collectionsMap => ({
+export const fetchCollectionsSuccess = collectionsMap => ({
     type: ShopActionTypes.FETCH_COLLETIONS_SUCCESS,
     payload: collectionsMap
 });
 
-const fetchCollectionFailure = errorMessage => ({
+export const fetchCollectionsFailure = errorMessage => ({
     type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
     payload: errorMessage
 })
-
-export const fetchCollectionsStartAsync = () => {
-    return dispatch => {
-        const collectionRef = firestore.collection('collections');
-        dispatch(fetchCollectionsStart());
-        
-        collectionRef.get()
-        .then(snapshot => {
-            const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-            dispatch(fetchCollectionsSuccess(collectionMap))
-        })
-        .catch(error => dispatch(fetchCollectionFailure(error.message)));
-    }
-}
