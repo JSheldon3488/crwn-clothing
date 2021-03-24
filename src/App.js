75bae6245eddb976bede2d9e33a9ac38611fbf46
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-24 13:56:43 -0500

    Moved Email Sign-In into Redux Sagas Flow

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
-import { setCurrentUser } from './redux/user/users.actions'
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 
@@ -23,7 +20,6 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
 
 /*     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -64,8 +60,5 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 })
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/user/users.sagas.js
+++ src/redux/user/users.sagas.js
@@ -2,20 +2,38 @@ import { takeLatest, put, all, call } from 'redux-saga/effects'
 
 import UserActionTypes from './user.types'
 
-import { googleSignInSuccess, googleSignInFailure } from './users.actions'
+import { SignInSuccess, SignInFailure } from './users.actions'
 
 import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils'
+
+
+function* getSnapShotFromUserAuth(userAuth) {
+    try {
+        const userRef = yield call(createUserProfileDocument, userAuth)
+        const userSnapshot = yield userRef.get()
+        yield put(
+            SignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+        );
+    } catch(error) {
+        yield put(SignInFailure(error))
+    }
+}
 
 function* signInWithGoogle() {
     try {
         const { user } = yield auth.signInWithPopup(googleProvider)
-        const userRef = yield call(createUserProfileDocument, user)
-        const userSnapshot = yield userRef.get()
-        yield put(
-            googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
-        );
+        yield getSnapShotFromUserAuth(user)
     } catch(error) {
-        yield put(googleSignInFailure(error))
+        yield put(SignInFailure(error))
+    }
+}
+
+function* signInWithEmail({payload: {email, password}}) {
+    try {
+        const { user } = yield auth.signInWithEmailAndPassword(email, password);
+        yield getSnapShotFromUserAuth(user)
+    } catch (error) {
+        yield put(SignInFailure(error))
     }
 }
 
@@ -23,6 +41,13 @@ export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
 }
 
+export function* onEmailSignInStart() {
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
+}
+
 export function* userSagas() {
-    yield all([call(onGoogleSignInStart)])
+    yield all([
+        call(onGoogleSignInStart),
+        call(onEmailSignInStart)
+    ])
 }
