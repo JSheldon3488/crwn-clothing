54562e0faab961972df35a2d338a51322df5c4b7
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-27 14:25:21 -0500

    Minor Layour Changes in Stripe-Button and Server.js

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price*100;
     const publishableKey = 'pk_test_51IX7FcHhkqHIEjl3BDyq38hsKJoUX2TGJD7Wq34JrSNEdTlemwYiKA85OsUF1Hwow3nnvTHX9oX2ogGaBXkFEBkn00iKTcuLD0';
+    
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error))
+            alert('There was an issue with your payment. Please make sure you use the provided credit card.')
+        })
     }
 
     return (
--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -25,13 +23,18 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('Server running on port ',  port);
+})
+
 app.post('/payment', (req, res) => {
     const body = {
         source: req.body.token.id,
         amount: req.body.amount,
         currency: 'usd'
     };
-    
+
     stripe.charges.create(body, (stripeErr, stripeRes) => {
         if (stripeErr) {
             res.status(500).send({ error: stripeErr })
@@ -39,9 +42,4 @@ app.post('/payment', (req, res) => {
             res.status(200).send({ success: stripeRes })
         }
     })
-})
-
-app.listen(port, error => {
-    if (error) throw error;
-    console.log('Server running on port ',  port);
 })
