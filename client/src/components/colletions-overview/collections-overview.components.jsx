bb8f9e1d6b3704d43cb2600546ea9565ef059f45
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-29 15:48:28 -0500

    Converted collections-overview styling to styled components

--- client/src/components/colletions-overview/collections-overview.components.jsx
+++ client/src/components/colletions-overview/collections-overview.components.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect' 
 
-import './collections-overview.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
+import { CollectionsOverviewContainer } from './collection-overview.styles';
 
-const CollectionsOverview = ({ collections }) => (
-    <div className='collections-overview'>
+
+const CollectionsOverview = ({ collections, isLoading }) => (
+    <CollectionsOverviewContainer>
         {
             collections.map(({ id, ...otherCollectionProps }) => (
                 <CollectionPreview key={id} {...otherCollectionProps} />
             ))
         }
-    </div>
+    </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
