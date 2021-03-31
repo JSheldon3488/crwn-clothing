0d0e9f4486c0b8cf4ed31b49b0dcd0f9b2b91786
Justin Sheldon <JSheldon3488@Gmail.com>
2021-03-31 10:15:40 -0500

    Fixed menu-item annimation

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -6,6 +6,7 @@ import { MenuItemContainer, BackgroundImageContainer, ContentContainer, ContentT
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <MenuItemContainer size={size} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <BackgroundImageContainer
+            className='background-image'
             imageUrl={imageUrl}
         />
         <ContentContainer>
--- client/src/components/menu-item/menu-item.styles.jsx
+++ client/src/components/menu-item/menu-item.styles.jsx
@@ -11,7 +11,8 @@ export const MenuItemContainer = styled.div`
 	border: 1px solid black;
 	margin: 0 7.5px 15px;
 	overflow: hidden;
-	&:hover {
+	
+  &:hover {
 		cursor: pointer;
 		& .background-image {
 			transform: scale(1.1);
@@ -21,12 +22,15 @@ export const MenuItemContainer = styled.div`
 			opacity: 0.9;
 		}
 	}
-	&:first-child {
+	
+  &:first-child {
     margin-right: 7.5px;
   }
+  
   &:last-child {
     margin-left: 7.5px;
   }
+  
   @media screen and (max-width: 800px) {
     height: 200px;
   }
